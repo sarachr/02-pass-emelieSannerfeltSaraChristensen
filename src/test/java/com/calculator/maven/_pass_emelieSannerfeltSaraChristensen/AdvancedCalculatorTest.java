44a2ca34704c1857a31c22af4a231531b08c305b
Sara Kristensen <sara.kristensen01@gmail.com>
2021-03-29 10:45:53 +0200

    Added more tests

--- src/test/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/AdvancedCalculatorTest.java
+++ src/test/java/com/calculator/maven/_pass_emelieSannerfeltSaraChristensen/AdvancedCalculatorTest.java
@@ -5,10 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class AdvancedCalculatorTest {
+	AdvancedCalculator advancedCalculator = new AdvancedCalculator();
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertEquals(advancedCalculator.cube(4), 64,0);
+		assertEquals(advancedCalculator.square(10), 100,00);
+		assertEquals(advancedCalculator.squareRoot(9), 1,73); //rätt?
+		assertEquals(advancedCalculator.absoluteValue(-3), 3,0);
+		assertEquals(advancedCalculator.raisedToThePowerOf(1,6), 1,0);
+		
 	}
 
 }
